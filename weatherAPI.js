52571142a381215aae5d4361b3b73996b07c18f5
Sean Riedl <smriedl@gmail.com>
2021-09-29 20:26:59 -0400

    Delete commented code

--- weatherAPI.js
+++ weatherAPI.js
@@ -6,16 +6,8 @@ class Weather {
   async getWeather(city, state) {
     const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},US&appid=${this.id}`)
     
-    //let message
-        
-    // if(weatherResponse.status === '200') {
-    //   const weather = await weatherResponse.json();
-    //   return weather;
-    // } else {
-    //   const err = await weatherResponse.json();
-    //   message = err.message;
-    // }
     const weather = await weatherResponse.json();
+    
     return {
       weather
     }
